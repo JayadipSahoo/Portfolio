9480a3022afc50f725eadc681834e517ce01a743
Jaya-Shinra Tensei <102919369+JayadipSahoo@users.noreply.github.com>
2023-06-09 23:15:13 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <>
 
-      <div className="pb-[20px] pt-0 z-30 fixed top-0 left-0 right-0 bg-[#282C33]">
+      <div className="mb-[20px] pt-0 z-30 fixed top-0 left-0 right-0 bg-[#282C33]">
         <div className=" px-5 max-w-[1560px] mx-auto flex items-center justify-between pt-6 my-2">
           <div className="left flex gap-2 items-center font-bold text-white text-base">
             {/* logo */}
